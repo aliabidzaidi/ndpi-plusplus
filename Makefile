626f97a88b6ebb432ef9866d39da6f691bf090f9
Ali Abid Zaidi <abidzaidi7@hotmail.com>
2022-05-19 20:42:22 +0400

    integrating pcap++ with ndpi & init port

--- Makefile
+++ Makefile
@@ -1,12 +1,14 @@
 include /usr/local/etc/PcapPlusPlus.mk
 
 # nDPI includes
-NDPI_INCLUDES=-I/opt/nDPI/src/include
+NDPI_LINKER=/opt/nDPI/src/lib/libndpi.a
 
+EXTRAS=-W -Wall -Wno-unused-parameter -Wno-unused-function -Wno-address-of-packed-member -g -O3
+EXTRAS2=-W -Wall -Wno-unused-parameter -Wno-unused-function -Wno-address-of-packed-member -g -O3 
 # All Target
 all:
-	g++ -fPIC -DPIC -g -I/opt/nDPI/src/include  -W -Wall -Wno-unused-parameter -Wno-unused-function -Wno-address-of-packed-member -g -O2  -pthread   -c -o main.o main.cpp
-	g++ -fPIC -DPIC -g -I/opt/nDPI/src/include  -W -Wall -Wno-unused-parameter -Wno-unused-function -Wno-address-of-packed-member -g -O2  -pthread   -c -o main.o main.cpp
+	g++ $(PCAPPP_BUILD_FLAGS) $(PCAPPP_INCLUDES) -g -I/opt/nDPI/src/include -c -o main.o main.cpp
+	g++ $(PCAPPP_LIBS_DIR) -fPIC -DPIC -g -I/opt/nDPI/src/include -o ndpi-plusplus main.o $(PCAPPP_LIBS) $(NDPI_LINKER)
 
 # Clean Target
 clean:
--- main.cpp
+++ main.cpp
@@ -1,8 +1,10 @@
 #include <iostream>
-
+#include "getopt.h"
 #include "stdlib.h"
-// #include "Packet.h"
-// #include "SystemUtils.h"
+#include "Packet.h"
+#include "SystemUtils.h"
+#include "PcapLiveDeviceList.h"
+
 
 #include <ndpi_api.h>
 #include <ndpi_main.h>
@@ -10,8 +12,53 @@
 
 struct ndpi_detection_module_struct *ndpi_struct;
 
+void printUsage()
+{
+    std::cout
+        << "-i    :   Port (eth0 or ens1)" << std::endl
+        << "--N   :   Number of maximum packets to send in nDPI" << std::endl
+        << std::endl;
+}
+
+std::string interface;
+int maxPackets;
+
 void processArgs(int argc, char **argv)
 {
+    const char *const short_opts = "i:Nh";
+    const option long_opts[] = {
+        {"i", required_argument, nullptr, 'i'},
+        {"N", required_argument, nullptr, 'N'},
+         {0, 0, 0, 0}
+        // {"", optional_argument, 0, ''},
+    };
+
+    while (true)
+    {
+        const auto opt = getopt_long(argc, argv, short_opts, long_opts, nullptr);
+
+        if (-1 == opt)
+            break;
+
+        switch (opt)
+        {
+        case 'i':
+            interface = std::string(optarg);
+            std::cout << "Input port set to: " << interface << std::endl;
+            break;
+
+        case 'N':
+            maxPackets = std::stoi(optarg);
+            std::cout << "Maximum packets to send in nDPI set to: " << maxPackets << std::endl;
+            break;
+
+        case 'h': // -h or --help
+        case '?': // Unrecognized option
+        default:
+            printUsage();
+            break;
+        }
+    }
 }
 
 void freeWorkflow()
@@ -39,4 +86,23 @@ int main(int argc, char **argv)
     ndpi_set_protocol_detection_bitmask2(ndpi_struct, &protos);
 
     ndpi_finalize_initialization(ndpi_struct);
+
+    pcpp::PcapLiveDevice *dev = pcpp::PcapLiveDeviceList::getInstance().getPcapLiveDeviceByName(interface);
+    if (dev == NULL)
+    {
+        std::cerr << "Cannot find interface '" << interface << "'" << std::endl;
+        return 1;
+    }
+
+    // before capturing packets let's print some info about this interface
+    std::cout
+        << "Interface info:" << std::endl
+        << "   Interface name:        " << dev->getName() << std::endl           // get interface name
+        << "   Interface description: " << dev->getDesc() << std::endl           // get interface description
+        << "   MAC address:           " << dev->getMacAddress() << std::endl     // get interface MAC address
+        << "   Default gateway:       " << dev->getDefaultGateway() << std::endl // get default gateway
+        << "   Interface MTU:         " << dev->getMtu() << std::endl;           // get interface MTU
+
+    if (dev->getDnsServers().size() > 0)
+        std::cout << "   DNS server:            " << dev->getDnsServers().at(0) << std::endl;
 }
